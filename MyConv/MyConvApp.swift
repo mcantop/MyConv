cf7f9bfbb4e277a1dfad1aab860658934fed2534
mcantop <maciejcantop@icloud.com>
2022-07-24 14:52:19 +0200

    Initial Commit

---
+++ MyConv/MyConvApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyConvApp.swift
+//  MyConv
+//
+//  Created by Maciej on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyConvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
